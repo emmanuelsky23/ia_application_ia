307f75fd2eef7e10b08ca7893844e86d758d9d22
emmanuelsky23 <nsaemmanuel91@gmail.com>
2025-02-09 13:41:48 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+
+# Titre de l'application
+st.title("Prédiction des Maladies avec IA")
+
+# Uploader le fichier CSV
+uploaded_file = st.file_uploader("Téléchargez votre fichier Training.csv", type="csv")
+
+if uploaded_file is not None:
+    # Charger les données
+    train_data = pd.read_csv(uploaded_file)
+
+    # Préparer les données
+    X = train_data.drop(['prognosis', 'Unnamed: 133'], axis=1)
+    y = train_data['prognosis']
+
+    # Entraîner le modèle
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    joblib.dump(model, "modele_prediction.pkl")
+
+    # Charger le modèle
+    model = joblib.load("modele_prediction.pkl")
+
+    # Entrée des symptômes
+    symptoms = st.text_input("Entrez vos symptômes séparés par des virgules")
+
+    if st.button("Prédire"):
+        if symptoms:
+            symptom_list = symptoms.split(",")
+            input_data = pd.DataFrame(columns=X.columns)
+            input_data.loc[0] = [1 if symptom.strip() in symptom_list else 0 for symptom in X.columns]
+            prediction = model.predict(input_data)
+            st.write(f"La maladie prédite est : {prediction[0]}")
+        else:
+            st.warning("Veuillez entrer au moins un symptôme.")
